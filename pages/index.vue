6696dad86d5ffc24640cfa0ae8febc5e3b4e6014
RamonMoraesyyz <ramon.k13@gmail.com>
2024-01-21 12:03:43 -0300

    estilização das páginas

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,41 @@
 <template>
-    <div class="relative bg-black text-white flex flex-col items-center justify-center h-screen overflow-hidden">
-      <div class="flex items-center justify-center">
-        <h1 class="text-4xl font-bold mb-4 mt-12">Cyberpunk cowboy n°1</h1>
-      </div>
-      <img src="/cyberpunkcowboy.png" class="w-100 h-full object-cover transform scale-70" />
-      
-       <!-- Player de música na parte inferior -->
-       <div class="fixed bottom-0 w-full flex justify-center">
-           <audio controls>
-           <source src="../public/cyberpunkcowboyn1110124.mp3" type="audio/mpeg">
-           </audio>
-       </div>
+  <div class="relative bg-black text-white flex h-screen overflow-hidden">
+
+    <!-- Conteúdo esquerdo (imagem) -->
+    <div class="w-1/2">
+      <img src="/cyberpunkcowboy.png" class="w-full h-full object-cover transform scale-70" />
     </div>
- </template>
+
+    <!-- Conteúdo direito (título, data, detalhes) -->
+    <div class="w-1/2 p-8 flex flex-col justify-center">
+
+      <!-- Título -->
+      <h1 class="text-4xl font-bold mb-10 mt-1">Cyberpunk cowboy n°1</h1>
+
+      <!-- Campo para a data da produção -->
+      <div class="mb-4">
+        <label class="text-lg">Produção: 11/01/2024</label>
+        <input type="text" class="bg-black border-b-2 border-white text-white p-2 w-full">
+      </div>
+
+      <!-- Campo para detalhes -->
+      <div class="mb-4">
+        <label class="text-lg"> Em um futuro distante, Steel Rider, o cowboy cyberpunk, percorre as ruas de uma cidade futurística em seu cavalo de metal, 
+          tocando blues noturno nos bares decadentes. 
+          Com seu chapéu sombreado e olhos cibernéticos cintilantes, 
+          ele é mais do que um músico; é um guardião das histórias esquecidas da humanidade. 
+          Em um mundo onde a tecnologia e a solidão dominam, Steel Rider personifica a resistência, mantendo viva a tradição do blues e encontrando a humanidade nas notas de sua guitarra elétrica e nos relinchos metálicos de seu leal companheiro. </label>
+        
+      </div>
+
+      <!-- Campo para o player de música -->
+      <div>
+        
+        <audio controls class="w-full mt-10">
+          <source src="../public/cyberpunkcowboyn1110124.mp3" type="audio/mpeg">
+        </audio>
+      </div>
+      
+    </div>
+  </div>
+</template>
--- pages/uriki.vue
+++ pages/uriki.vue
@@ -1,15 +1,40 @@
 <template>
-    <div class="relative bg-black text-white flex flex-col items-center justify-center h-screen overflow-hidden">
-      <div class="flex items-center justify-center">
-        <h1 class="text-4xl font-bold mb-4 mt-12">Uriki indians n°2</h1>
+    <div class="relative bg-black text-white flex h-screen overflow-hidden">
+  
+      <!-- Conteúdo esquerdo (imagem) -->
+      <div class="w-1/2">
+        <img src="../public/indiansuriki.jpeg" class="w-full h-full object-cover transform scale-70" />
       </div>
-      <img src="../public/indiansuriki.jpeg" class="w-100 h-full object-cover transform scale-70" />
-      
-       <!-- Player de música na parte inferior -->
-       <div class="fixed bottom-0 w-full flex justify-center">
-           <audio controls>
-           <source src="../public/uriki.aac" type="audio/mpeg">
-           </audio>
-       </div>
+  
+      <!-- Conteúdo direito (título, data, detalhes) -->
+      <div class="w-1/2 p-8 flex flex-col justify-center">
+  
+        <!-- Título -->
+        <h1 class="text-4xl font-bold mb-10 mt-1">Indians Uriki n°2</h1>
+  
+        <!-- Campo para a data da produção -->
+        <div class="mb-4">
+          <label class="text-lg">Produção: 20/01/2024</label>
+          <input type="text" class="bg-black border-b-2 border-white text-white p-2 w-full">
+        </div>
+  
+        <!-- Campo para detalhes -->
+        <div class="mb-4">
+          <label class="text-lg"> No coração da densa floresta tropical, o povo indígena Uriki praticava um ritual sagrado à luz da lua, onde, ao som de tambores e flautas, evocavam os espíritos de seus antepassados.
+             Vestidos com ornamentos de penas, dançavam em círculo, entoando cânticos ancestrais para estabelecer uma ligação transcendental com a natureza e seus antigos guias espirituais. 
+             Liderados por um ancião, o ritual atingia seu ápice, dissipando as fronteiras entre o mundo terreno e espiritual. Os Uriki acreditavam que, nesses momentos, a sabedoria ancestral era compartilhada pelos espíritos, proporcionando orientação e bênçãos para a tribo. 
+             Ao nascer do sol, agradeciam aos espíritos, mantendo viva a chama da espiritualidade e harmonia com a natureza para as gerações futuras. </label>
+          
+        </div>
+  
+        <!-- Campo para o player de música -->
+        <div>
+          
+          <audio controls class="w-full">
+            <source src="../public/uriki.aac" type="audio/mpeg">
+          </audio>
+        </div>
+        
+      </div>
     </div>
- </template>
+  </template>
